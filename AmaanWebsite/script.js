3c22a2b5a651800d88d9808a7d2288d8c4a7efdc
AmaanSyed2002 <103594853+AmaanSyed2002@users.noreply.github.com>
2023-08-14 10:25:48 -0500

    Add files via upload

---
+++ AmaanWebsite/script.js
@@ -0,0 +1,7 @@
+function showContent() {
+  var content = document.querySelector('.content');
+  var profilePic = document.querySelector('.profile-pic');
+
+  content.style.display = 'block';
+  profilePic.style.display = 'block';
+}
